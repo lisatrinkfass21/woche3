609c4dc699b5b5789fa807f8752640b8394cf567
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-10-06 22:05:46 +0200

    commit 2.5

--- Beispiel2/src/beispiel2/Main.java
+++ Beispiel2/src/beispiel2/Main.java
@@ -5,8 +5,14 @@
  */
 package beispiel2;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -17,7 +23,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int[] randomNumbers = new int[1000];
         String[] randomStrings = new String[10];
@@ -32,6 +38,21 @@ public class Main {
             randomStrings[i] = new String(arr, Charset.forName("UTF-8"));
 
         }
+
+        List<Weapon> list = new LinkedList<>();
+        list = Files.lines(new File("weapons.csv").toPath())
+                .skip(1)
+                .map(s -> s.split(";"))
+                .map(s -> new Weapon(
+                s[0],
+                CombatType.valueOf(s[1]),
+                DamageType.valueOf(s[2]),
+                Integer.parseInt(s[3]),
+                Integer.parseInt(s[4]),
+                Integer.parseInt(s[5]),
+                Integer.parseInt(s[6])
+        ))
+                .collect(Collectors.toList());
 
     }
 
